340790d82512b7f75176c7834efd6245f255df7f
Gustavo Muñoz <timbergus@gmail.com>
2023-08-06 10:47:33 +0200

    Make number optional.

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -6,56 +6,62 @@ export const schema = z.object({
     .string()
     .min(3)
     .max(10)
+    .or(z.nan())
     .or(z.literal(''))
     .or(z.literal(null))
     .or(z.literal(undefined))
     .refine((value) => !isRequired('firstName') || value, {
       message: 'This field is required',
     })
-    .transform((value) => value ?? null),
+    .transform((value) => (value ? value : null)),
   lastName: z
     .string()
     .min(3)
     .max(10)
+    .or(z.nan())
     .or(z.literal(''))
     .or(z.literal(null))
     .or(z.literal(undefined))
     .refine((value) => !isRequired('lastName') || value, {
       message: 'This field is required',
     })
-    .transform((value) => value ?? null),
+    .transform((value) => (value ? value : null)),
   dateOfBirth: z
     .date()
+    .or(z.nan())
     .or(z.literal(''))
     .or(z.literal(null))
     .or(z.literal(undefined))
     .refine((value) => !isRequired('dateOfBirth') || value, {
       message: 'This field is required',
     })
-    .transform((value) => value ?? null),
+    .transform((value) => (value ? value : null)),
   age: z
     .number()
+    .int()
     .positive()
     .min(18)
+    .or(z.nan())
     .or(z.literal(''))
     .or(z.literal(null))
     .or(z.literal(undefined))
     .refine((value) => !isRequired('age') || value, {
       message: 'This field is required',
     })
-    .transform((value) => value ?? null),
+    .transform((value) => (value ? value : null)),
   email: z
     .string()
     .email({
       message: 'This email is invalid',
     })
+    .or(z.nan())
     .or(z.literal(''))
     .or(z.literal(null))
     .or(z.literal(undefined))
     .refine((value) => !isRequired('email') || value, {
       message: 'This field is required',
     })
-    .transform((value) => value ?? null),
+    .transform((value) => (value ? value : null)),
 })
 
 export type Values = z.infer<typeof schema>
